d37bfb005b3335bd0bfa3151466a5d7687d5546f
Lenshen <779822644@qq.com>
2015-09-04 09:30:15 +0800

    add object
    add object

---
+++ 蜗牛校园/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  蜗牛校园
+//
+//  Created by 远深 on 15/9/4.
+//  Copyright (c) 2015年 yance.bao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 蜗牛校园/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  蜗牛校园
+//
+//  Created by 远深 on 15/9/4.
+//  Copyright (c) 2015年 yance.bao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
